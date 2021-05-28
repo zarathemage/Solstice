0864b825d393a5fadc2fe948c689141cd6db4577
zarathemage <74919281+zarathemage@users.noreply.github.com>
2021-05-29 00:25:03 +0200

    Add files via upload

--- solstice-macos/underdarkroom2.py
+++ solstice-macos/underdarkroom2.py
@@ -1,12 +1,14 @@
 import pygame
 
 from room import *
-from underdarkroom2tilemap import *
+from underdarkroom1tilemap import *
+
+from movethroughdoorloopobject import *
 
 class UnderdarkRoom2(Room):
     def __init__(self):
         Room.__init__(self,0,0)
-        self.image = pygame.image.load("./pics/solstice-roomcrossroad-0.png")
+        self.image = pygame.image.load("./pics/crossroadroom1-bg-3.png")
 
         self.tilemap = UnderdarkRoom2Tilemap()
         
@@ -14,13 +16,24 @@ class UnderdarkRoom2(Room):
         1 ### FIXME
 
     def blit(self, screen):
-
-        ###        screen.blit(self.image, [self.x, self.y])
+###        screen.blit(self.image, [186,55])
 
         self.tilemap.blit(screen)
 
-    def exit(self, player):
-        if player.x > 500: ### FIXME
-            return None###FIXME Room3()
+
+    def exit(self, player, screen):
+        if player.x > 350: ### FIXME
+            ### play movie of walking through door
+            o = MoveThroughDoorLoopObject()
+            o.move_mainloop(player, self, screen)
+            
+            return UnderdarkRoom2()
+        elif player.x < 83 and player.y > 205 and player.y < 305:
+            ### play movie of walking through door
+            o = MoveThroughDoorLoopObject()
+            o.move_mainloop(player, self, screen)            
+
+            return UnderdarkRoom1()
         else:
             return None
+
